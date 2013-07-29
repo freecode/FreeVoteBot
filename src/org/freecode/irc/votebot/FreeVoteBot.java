41433861050a9de91d3a8f41dbc6fdd5906926d8
Shivam Mistry <shivam9000@gmail.com>
2013-07-29 21:36:08 +0100

    votes and polls tables separated

--- src/org/freecode/irc/votebot/FreeVoteBot.java
+++ src/org/freecode/irc/votebot/FreeVoteBot.java
@@ -10,6 +10,8 @@ import org.freecode.irc.event.PrivateMessageListener;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * User: Shivam
@@ -34,8 +36,8 @@ public class FreeVoteBot implements PrivateMessageListener {
 			dbConn = DriverManager.getConnection("jdbc:sqlite:freecode.db");
 			Statement statement = dbConn.createStatement();
 			statement.setQueryTimeout(5);
-			statement.executeUpdate("CREATE TABLE IF NOT EXISTS poll (id integer PRIMARY KEY AUTOINCREMENT, question " +
-					"string NOT NULL, yesVotes integer DEFAULT 0, noVotes integer DEFAULT 0, abstainVotes integer DEFAULT 0)");
+			statement.executeUpdate("CREATE TABLE IF NOT EXISTS polls (id integer PRIMARY KEY AUTOINCREMENT, question string NOT NULL, options string NOT NULL DEFAULT 'yes,no,abstain')");
+			statement.executeUpdate("CREATE TABLE IF NOT EXISTS votes (pollId integer, voter string NOT NULL, answerIndex integer NOT NULL)");
 
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
@@ -126,7 +128,7 @@ public class FreeVoteBot implements PrivateMessageListener {
 		if (privmsg.getMessage().startsWith("!createpoll")) {
 			String msg = privmsg.getMessage().substring("!createpoll".length()).trim();
 			try {
-				PreparedStatement statement = dbConn.prepareStatement("INSERT INTO poll(question) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+				PreparedStatement statement = dbConn.prepareStatement("INSERT INTO polls(question) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
 				statement.setString(1, msg.trim());
 				statement.execute();
 				ResultSet rs = statement.getGeneratedKeys();
@@ -142,52 +144,123 @@ public class FreeVoteBot implements PrivateMessageListener {
 			System.out.println(msg);
 			String[] split = msg.split(" ", 2);
 			if (split.length == 2) {
-				String id = split[0];
+				String ids = split[0];
 				String vote = split[1].toLowerCase();
 				if (!vote.equalsIgnoreCase("yes") && !vote.equalsIgnoreCase("no") && !vote.equalsIgnoreCase("abstain")) {
 					return;
 				}
+				int nId;
+				if (vote.equalsIgnoreCase("yes")) {
+					nId = 0;
+				} else if (vote.equalsIgnoreCase("no")) {
+					nId = 1;
+				} else {
+					nId = 2;
+				}
+				if (!ids.matches("\\d+")) {
+					return;
+				}
+				int id = Integer.parseInt(ids);
+				try {
+					ResultSet rs;
+					PreparedStatement statement;
+					statement = dbConn.prepareStatement("SELECT * FROM polls WHERE id = ?");
+					statement.setInt(1, id);
+					rs = statement.executeQuery();
+					if (rs.next()) {
+						statement = dbConn.prepareStatement("SELECT * FROM votes WHERE voter = ? AND pollId = ?");
+						statement.setString(1, privmsg.getNick());
+						statement.setInt(2, id);
+						rs = statement.executeQuery();
+						if (rs.next()) {
+							PreparedStatement stmt = dbConn.prepareStatement("UPDATE votes SET answerIndex = ? WHERE voter = ? AND pollId = ?");
+							stmt.setInt(1, nId);
+							stmt.setString(2, privmsg.getNick());
+							stmt.setInt(3, id);
+							stmt.execute();
+							privmsg.getIrcConnection().send(new Privmsg(privmsg.getTarget(), "Vote updated", privmsg.getIrcConnection()));
+						} else {
+							PreparedStatement stmt = dbConn.prepareStatement("INSERT INTO votes(pollId,voter,answerIndex) VALUES (?,?,?)");
+							stmt.setInt(1, id);
+							stmt.setString(2, privmsg.getNick());
+							stmt.setInt(3, nId);
+							stmt.execute();
+							privmsg.getIrcConnection().send(new Privmsg(privmsg.getTarget(), "Vote cast", privmsg.getIrcConnection()));
+
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if (split.length == 1) {
+				String id = split[0];
 				if (!id.matches("\\d+")) {
 					return;
 				}
 				try {
-					PreparedStatement statement = dbConn.prepareStatement("SELECT * FROM poll WHERE id = ?");
+					ResultSet rs;
+					PreparedStatement statement;
+					statement = dbConn.prepareStatement("SELECT * FROM polls WHERE id = ?");
 					statement.setInt(1, Integer.parseInt(id));
-
-					ResultSet rs = statement.executeQuery();
+					rs = statement.executeQuery();
+					String question = null;
+					String[] options = null;
 					if (rs.next()) {
-						PreparedStatement stmt = null;
-						int yes = rs.getInt("yesVotes"), no = rs.getInt("noVotes"), abstain = rs.getInt("abstainVotes");
-						if (vote.equals("yes")) {
-							yes++;
-							stmt = dbConn.prepareStatement("UPDATE poll SET yesVotes = ? WHERE id = ?");
-							stmt.setInt(1, yes);
-							stmt.setInt(2, Integer.parseInt(id));
-						} else if (vote.equals("no")) {
-							no++;
-							stmt = dbConn.prepareStatement("UPDATE poll SET noVotes = ? WHERE id = ?");
-							stmt.setInt(1, no);
-							stmt.setInt(2, Integer.parseInt(id));
-						} else {
-							abstain++;
-							stmt = dbConn.prepareStatement("UPDATE poll SET abstainVotes = ? WHERE id = ?");
-							stmt.setInt(1, abstain);
-							stmt.setInt(2, Integer.parseInt(id));
-						}
-						stmt.execute();
-
-						privmsg.getIrcConnection().send(new Privmsg(privmsg.getTarget(), "Vote cast for "
-								+ rs.getString("question") + ", current stats: yes: " + yes + " no: " + no +
-								" abstain: " + abstain, privmsg.getIrcConnection()));
-
-
+						question = rs.getString("question");
+						options = stringToArray(rs.getString("options"));
 					}
-
+					if (question != null) {
+						statement = dbConn.prepareStatement("SELECT * FROM votes WHERE pollId = ?");
+						statement.setInt(1, Integer.parseInt(id));
+						rs = statement.executeQuery();
+						int yes = 0, no = 0, abstain = 0;
+						while (rs.next()) {
+							int i = rs.getInt("answerIndex");
+							if (i == 0) {
+								yes++;
+							} else if (i == 1) {
+								no++;
+							} else if (i == 2) {
+								abstain++;
+							}
+						}
+						privmsg.getIrcConnection().send(new Privmsg(privmsg.getTarget(), "Poll: " + question +
+								" Options: " + Arrays.toString(options) + " Yes: " + yes + " No: " + no + " Abstain: "
+								+ abstain, privmsg.getIrcConnection()));
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 
 		}
+	}
+
+	private String[] stringToArray(String users) {
+		if (users == null || users.length() == 0)
+			return new String[0];
+		ArrayList<String> voters = new ArrayList<String>();
+		int start = 0;
+		for (int i = 1; i < users.length(); i++) {
+			char c = users.charAt(i);
+			if (c == ',' || i == users.length() - 1) {
+				if (i - 1 > 0 && users.charAt(i - 1) == '\\' && i != users.length() - 1) {
+					continue;
+				} else {
+					String str = users.substring(start, i == users.length() - 1 ? i + 1 : i );
+					voters.add(str);
+					start = i + 1;
+				}
+			}
+		}
+		return voters.toArray(new String[voters.size()]);
+	}
+
+	public String arrayToString(String[] users) {
+		StringBuilder builder = new StringBuilder();
+		for (String s : users) {
+			builder.append(s).append(',');
+		}
+		return builder.substring(0, builder.lastIndexOf(","));
 	}
 }
