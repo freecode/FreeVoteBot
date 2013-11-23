b3e290642c9f5315b08b39816318f945a2d5900b
imDeprecated <rsbdeprecated@live.com>
2013-11-22 22:19:28 -0500

    Refactored CreatePollModule for better readability

--- src/main/java/org/freecode/irc/votebot/modules/admin/CreatePollModule.java
+++ src/main/java/org/freecode/irc/votebot/modules/admin/CreatePollModule.java
@@ -1,17 +1,16 @@
 package org.freecode.irc.votebot.modules.admin;
 
-import org.freecode.irc.Notice;
 import org.freecode.irc.Privmsg;
-import org.freecode.irc.votebot.FreeVoteBot;
-import org.freecode.irc.votebot.NoticeFilter;
-import org.freecode.irc.votebot.api.CommandModule;
+import org.freecode.irc.votebot.api.AdminModule;
 import org.freecode.irc.votebot.dao.PollDAO;
 
 import java.sql.SQLException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class CreatePollModule extends CommandModule {
+public class CreatePollModule extends AdminModule {
+    private static final long DEFAULT_LIFE_SPAN = 604800000L;
+    public static final String CREATE_POLL_WITH_LIFESPAN_PATTERN = "!createpoll \\d{1,6}[whsdmWHSDM]? .+";
+    public static final String LIFESPAN_PATTERN = "\\d{1,6}[whsdmWHSDM]?";
+
     private PollDAO pollDAO;
 
     @Override
@@ -19,60 +18,43 @@ public class CreatePollModule extends CommandModule {
         if (privmsg.getMessage().trim().equals("!createpoll")) {
             return;
         }
-        long txp = 604800 * 1000;
-        final String msg;
-        if (privmsg.getMessage().matches("!createpoll \\d{1,6}[whsdmWHSDM]? .+")) {
-            final String[] parts = privmsg.getMessage().split(" ", 3);
+
+        long lifeSpan = DEFAULT_LIFE_SPAN;
+        final String question;
+        if (privmsg.getMessage().matches(CREATE_POLL_WITH_LIFESPAN_PATTERN)) {
             try {
-                txp = parseExpiry(parts[1]);
-            } catch (Exception e) {
-                e.printStackTrace();
+                final String[] parts = privmsg.getMessage().split(" ", 3);
+                lifeSpan = parseExpiry(parts[1]);
+                question = parts[2];
+            } catch (IllegalArgumentException e) {
+                privmsg.getIrcConnection().send(new Privmsg(privmsg.getTarget(), e.getMessage(), privmsg.getIrcConnection()));
+                throw e;
             }
-            msg = parts[2];
         } else {
             final String[] parts = privmsg.getMessage().split(" ", 2);
-            msg = parts[1];
+            question = parts[1];
         }
-        final long exp = System.currentTimeMillis() + txp;
-        if (msg.isEmpty() || msg.length() < 5) {
+
+        if (question.isEmpty() || question.length() < 5) {
             privmsg.getIrcConnection().send(new Privmsg(privmsg.getTarget(), "Question is too short.", privmsg.getIrcConnection()));
             return;
         }
-        privmsg.getIrcConnection().addListener(new NoticeFilter() {
-            public boolean accept(Notice notice) {
-                Pattern pattern = Pattern.compile("\u0002(.+?)\u0002");
-                Matcher matcher = pattern.matcher(notice.getMessage());
-                if (matcher.find() && matcher.find()) {
-                    String access = matcher.group(1);
-                    System.out.println(access);
-                    if (access.equals("AOP") || access.equals("Founder") || access.equals("SOP") || access.equals("HOP")) {
-                        return notice.getNick().equals("ChanServ") && notice.getMessage().contains("Main nick:") && notice.getMessage().contains("\u0002" + privmsg.getNick() + "\u0002");
-                    }
-                }
-                if (notice.getMessage().equals("Permission denied."))
-                    notice.getIrcConnection().removeListener(this);
-                return false;
-            }
 
-            public void run(Notice notice) {
-                try {
-                    String mainNick = notice.getMessage().substring(notice.getMessage().indexOf("Main nick:") + 10).trim();
-                    int id = pollDAO.addNewPoll(msg.trim(), exp, mainNick);
-                    privmsg.getIrcConnection().send(new Privmsg(privmsg.getTarget(), "Created poll, type !vote " + id + " yes/no/abstain to vote.", privmsg.getIrcConnection()));
-                    privmsg.getIrcConnection().removeListener(this);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        privmsg.getIrcConnection().send(new Privmsg("ChanServ", "WHY " + FreeVoteBot.CHANNEL_SOURCE + " " + privmsg.getNick(), privmsg.getIrcConnection()));
+        try {
+            final long expiration = System.currentTimeMillis() + lifeSpan;
+            int id = pollDAO.addNewPoll(question.trim(), expiration, privmsg.getNick());
+            privmsg.getIrcConnection().send(new Privmsg(privmsg.getTarget(), "Created poll, type !vote " + id + " yes/no/abstain to vote.", privmsg.getIrcConnection()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    private long parseExpiry(String expiry) {
-        if (expiry.matches("\\d{1,6}[whsdmWHSDM]?")) {
+    private long parseExpiry(String lifespan) {
+        if (lifespan.matches(LIFESPAN_PATTERN)) {
             long multiplier = 1000;
-            if (Character.isLetter(expiry.charAt(expiry.length() - 1))) {
-                char c = Character.toLowerCase(expiry.charAt(expiry.length() - 1));
+
+            if (Character.isLetter(lifespan.charAt(lifespan.length() - 1))) {
+                char c = Character.toLowerCase(lifespan.charAt(lifespan.length() - 1));
                 switch (c) {
                     case 'w':
                         multiplier *= 604800;
@@ -86,14 +68,12 @@ public class CreatePollModule extends CommandModule {
                     case 'd':
                         multiplier *= 86400;
                         break;
-                    default:
-                        break;
                 }
-                expiry = expiry.substring(0, expiry.length() - 1);
+                lifespan = lifespan.substring(0, lifespan.length() - 1);
             }
-            return Long.parseLong(expiry) * multiplier;
+            return Long.parseLong(lifespan) * multiplier;
         } else {
-            throw new IllegalArgumentException("too big");
+            throw new IllegalArgumentException("CreatePollModule - Given lifespan is incorrectly formatted.");
         }
     }
 
@@ -101,7 +81,6 @@ public class CreatePollModule extends CommandModule {
     public String getName() {
         return "createpoll";
     }
-
 
     @Override
     public String getParameterRegex() {
