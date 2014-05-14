860926931bbf3057a1f7794f14a3b446126d2fe5
Joel <joel.j.mills@gmail.com>
2014-05-14 19:21:40 +0100

    Bug fix & use same format

--- src/main/java/org/freecode/irc/votebot/PollExpiryAnnouncer.java
+++ src/main/java/org/freecode/irc/votebot/PollExpiryAnnouncer.java
@@ -26,48 +26,46 @@ public class PollExpiryAnnouncer implements Runnable {
 
     public void run() {
         long ttl = expiry - System.currentTimeMillis();
-        String text = "Poll #%d (\"%s\") has less than %s remaining!";
+        String time_announcement = "Poll #%d has less than %s remaining!";
 
         try {
             Poll poll = fvb.getPollDAO().getPoll(id);
             String question = poll.getQuestion();
+            Vote[] votes = fvb.getVoteDAO().getVotesOnPoll(id);
+            int total = votes.length;
+            int yes = 0, no = 0, abstain = 0;
+            for (Vote v : votes) {
+                switch (v.getAnswerIndex()) {
+                    case Vote.YES:
+                        yes++;
+                        break;
+                    case Vote.NO:
+                        no++;
+                        break;
+                    case Vote.ABSTAIN:
+                        abstain++;
+                        break;
+                    default:
+                        break;
+                }
+            }
 
             if ((hasAnnounced & 1) == 0 && ttl <= 2 * 300000 && ttl >= 0) {
                 hasAnnounced |= 1;
-                fvb.sendMsg(String.format(text, id, question, "10 minutes"));
+                fvb.sendMsg(String.format(time_announcement, id, question, "10 minutes"));
             } else if ((hasAnnounced & 3) == 0 && ttl <= MILLIS_IN_AN_HOUR * 2 && ttl >= 0) {
                 hasAnnounced |= 2;
-                fvb.sendMsg(String.format(text, id, question, "two hours"));
+                fvb.sendMsg(String.format(time_announcement, id, question, "two hours"));
             } else if ((hasAnnounced & 7) == 0 && ttl <= 12 * MILLIS_IN_AN_HOUR && ttl >= 0) {
                 hasAnnounced |= 4;
-                fvb.sendMsg(String.format(text, id, question, "twelve hours"));
+                fvb.sendMsg(String.format(time_announcement, id, question, "twelve hours"));
             } else if (ttl <= 0 && ((hasAnnounced & Integer.MAX_VALUE) != Integer.MAX_VALUE)) {
                 hasAnnounced = Integer.MAX_VALUE;
-                getFuture().cancel(true);
-                Vote[] votes = fvb.getVoteDAO().getVotesOnPoll(id);
-                int total = votes.length;
-                int yes = 0, no = 0, abstain = 0;
-                for (Vote v : votes) {
-                    switch (v.getAnswerIndex()) {
-                        case Vote.YES:
-                            yes++;
-                            break;
-                        case Vote.NO:
-                            no++;
-                            break;
-                        case Vote.ABSTAIN:
-                            abstain++;
-                            break;
-                        default:
-                            break;
-                    }
-                }
                 String result = (total >= 5 && yes > no && yes > abstain) ? "passed" : "did not pass";
                 fvb.sendMsg(String.format("Poll #%d %s!", id, result));
-                fvb.sendMsg(String.format("Question: \"%s\", Yes: %d, No: %d, Abstain: %d", question, yes, no, abstain));
                 getFuture().cancel(true);
-
             }
+            fvb.sendMsg(String.format("Question: \"%s\", Yes: %d, No: %d, Abstain: %d", question, yes, no, abstain));
         } catch (SQLException e) {
             e.printStackTrace();
         }
