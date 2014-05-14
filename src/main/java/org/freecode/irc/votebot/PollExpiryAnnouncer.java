ac37bac2bd52d02b0ae6e82975f4956a64be9be7
Joel <joel.j.mills@gmail.com>
2014-05-14 19:00:46 +0100

    Show question on announce

--- src/main/java/org/freecode/irc/votebot/PollExpiryAnnouncer.java
+++ src/main/java/org/freecode/irc/votebot/PollExpiryAnnouncer.java
@@ -26,19 +26,24 @@ public class PollExpiryAnnouncer implements Runnable {
 
     public void run() {
         long ttl = expiry - System.currentTimeMillis();
-        if ((hasAnnounced & 1) == 0 && ttl <= 2 * 300000 && ttl >= 0) {
-            hasAnnounced |= 1;
-            fvb.sendMsg(String.format("Poll #%d has less than 10 minutes remaining!", id));
-        } else if ((hasAnnounced & 3) == 0 && ttl <= MILLIS_IN_AN_HOUR * 2 && ttl >= 0) {
-            hasAnnounced |= 2;
-            fvb.sendMsg(String.format("Poll #%d has less than two hours remaining!", id));
-        } else if ((hasAnnounced & 7) == 0 && ttl <= 12 * MILLIS_IN_AN_HOUR && ttl >= 0) {
-            hasAnnounced |= 4;
-            fvb.sendMsg(String.format("Poll #%d has less than twelve hours remaining!", id));
-        } else if (ttl <= 0 && ((hasAnnounced & Integer.MAX_VALUE) != Integer.MAX_VALUE)) {
-            hasAnnounced = Integer.MAX_VALUE;
-            try {
-                Poll poll = fvb.getPollDAO().getPoll(id);
+        String text = "Poll #%d (\"%s\") has less than %s remaining!";
+
+        try {
+            Poll poll = fvb.getPollDAO().getPoll(id);
+            String question = poll.getQuestion();
+
+            if ((hasAnnounced & 1) == 0 && ttl <= 2 * 300000 && ttl >= 0) {
+                hasAnnounced |= 1;
+                fvb.sendMsg(String.format(text, id, question, "10 minutes"));
+            } else if ((hasAnnounced & 3) == 0 && ttl <= MILLIS_IN_AN_HOUR * 2 && ttl >= 0) {
+                hasAnnounced |= 2;
+                fvb.sendMsg(String.format(text, id, question, "two hours"));
+            } else if ((hasAnnounced & 7) == 0 && ttl <= 12 * MILLIS_IN_AN_HOUR && ttl >= 0) {
+                hasAnnounced |= 4;
+                fvb.sendMsg(String.format(text, id, question, "twelve hours"));
+            } else if (ttl <= 0 && ((hasAnnounced & Integer.MAX_VALUE) != Integer.MAX_VALUE)) {
+                hasAnnounced = Integer.MAX_VALUE;
+                getFuture().cancel(true);
                 Vote[] votes = fvb.getVoteDAO().getVotesOnPoll(id);
                 int total = votes.length;
                 int yes = 0, no = 0, abstain = 0;
@@ -59,11 +64,11 @@ public class PollExpiryAnnouncer implements Runnable {
                 }
                 String result = (total >= 5 && yes > no && yes > abstain) ? "passed" : "did not pass";
                 fvb.sendMsg(String.format("Poll #%d %s!", id, result));
-                fvb.sendMsg(String.format("Question: \"%s\", Yes: %d, No: %d, Abstain: %d", poll.getQuestion(), yes, no, abstain));
-            } catch (SQLException e) {
-                e.printStackTrace();
+                fvb.sendMsg(String.format("Question: \"%s\", Yes: %d, No: %d, Abstain: %d", question, yes, no, abstain));
+
             }
-            getFuture().cancel(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
