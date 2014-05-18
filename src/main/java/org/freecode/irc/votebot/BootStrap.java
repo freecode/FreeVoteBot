08d24a50a262ff0fe24c89f93908666937728190
Marcin <pucilowski.m@gmail.com>
2014-05-18 20:37:02 +0100

    oops

--- src/main/java/org/freecode/irc/votebot/BootStrap.java
+++ src/main/java/org/freecode/irc/votebot/BootStrap.java
@@ -1,6 +1,10 @@
 package org.freecode.irc.votebot;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -9,10 +13,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Date: 11/21/13
  * Time: 11:26 PM
  */
+@Configuration
+@ComponentScan(basePackages = "org.freecode.irc.votebot")
+@ImportResource({"classpath:spring/application.xml", "classpath:spring/datasource.xml",})
 public class BootStrap {
-    public static void main(String... args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring/application.xml");
-        context.getBean("freeVoteBot");
-    }
 
+    public static void main(String[] args) {
+        ApplicationContext app = new AnnotationConfigApplicationContext(BootStrap.class);
+
+        FreeVoteBot fvb = app.getBean(FreeVoteBot.class);
+    }
 }
